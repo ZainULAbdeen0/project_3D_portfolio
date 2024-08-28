003933262caa84663061e97e7b87d705c296a46f
ZainULAbdeen0 <mujtabazain183@gmail.com>
2024-08-28 18:13:18 +0500

    A commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../style.js";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="w-full relative h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row gap-5 items-start`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi I&apos;m <span className="text-[#915eff]">ZainUlAbdeen</span>
+          </h1>
+          <p className={`${styles.heroSubText} text-white-100`}>Crafting Seamless Web <br className="sm:block hidden"/>Experiences with MERN & Next.js</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import {useEffect , useState} from 'react';
+import { Link } from 'react-router-dom';
+import {styles} from '../style';
+import {navLinks} from '../constants';
+import {logo , menu , close} from '../assets';
 
 const Navbar = () => {
+  const [Toogle, setToogle] = useState(false)
+  const [Active, setActive] = useState("")
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full fixed flex items-center py-5  top-0 z-20 bg-primary`}>
+      <div className="w-full flex items-center justify-between max-w-7xl mx-bottom">
+        <Link 
+        to="/" 
+        className='flex items-center gap-2' 
+        onClick={
+          ()=>{setActive("")
+          window.scrollTo(0.0)
+        }}>
+        <img src={logo} alt="Icon" className=" w-16 h-16 object-contain" />
+        <p className='text-white text-[18px] font-bold cursor-pointer'>ZAIN</p>
+        </Link>
+        <ul className="list-none hidden sm:flex gap-10 flex-row">
+          {navLinks.map((link)=>(
+            <li key={link.id}
+            className={`${ 
+              Active === link.title
+              ? "text-white"
+              : "text-secondary"
+              } text-[18px] hover:text-white font-medium cursor-pointer`}
+               onClick = {()=> setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>
+              {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        {/* mobile navigation  */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img 
+          src={Toogle ? close : menu} 
+          alt="manu"
+          className="sm:hidden h-[28px] w-[28px] object-contain cursor-pointer" 
+          onClick={()=> setToogle(!Toogle)}
+          />
+          <div className={`${!Toogle ? "hidden" : "flex absolute"} p-6 black-gradient top-20 mx-5 right-0 min-w-[140px] rounded-xl my-2 z-10`}>
+          <ul className="list-none flex flex-col justify-end text-start gap-4">
+          {navLinks.map((link)=>(
+            <li key={link.id}
+            className={`${ 
+              Active === link.title
+              ? "text-white"
+              : "text-secondary"
+              } text-[16px] font-medium font-poppins cursor-pointer`}
+               onClick = {()=> {
+                setToogle(!Toogle);
+                setActive(link.title)}}
+            >
+              <a href={`#${link.id}`}>
+              {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
