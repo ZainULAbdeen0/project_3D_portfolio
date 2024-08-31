8490a070347db2b3087b860980e4037dbf8f1351
ZainULAbdeen0 <mujtabazain183@gmail.com>
2024-08-31 20:54:36 +0500

    updated tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import {technologies} from '../constants'
+import {BallCanvas} from './canvas'
+import {SectionWrapper} from '../hoc'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-wrap flex-row gap-10 justify-center">
+      {technologies.map((tech)=>(
+        <div className="w-28 h-28" key={tech.name}>
+            <BallCanvas
+              icon = {tech.icon}
+            />
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech ,"")
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React , {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import CanvasLoader from '../Loader'
+import { Decal , Float , OrbitControls , Preload , useTexture} from '@react-three/drei'
 
-const Ball = () => {
+const Ball = ({icon}) => {
+  {console.log(icon)}
+const decal = useTexture(icon)
   return (
-    <div>Ball</div>
+    <Float floatIntensity={2} speed={0.75} rotationIntensity={1}>
+      <ambientLight intensity={1}/>
+      <directionalLight direction={[0,0,0.5]}/>
+      <mesh castShadow receiveShadow scale={2.5}>
+        <icosahedronGeometry args={[1,1]}/>
+        <meshStandardMaterial 
+          color = "#fff8eb"
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0,0,1]}
+          rotation={[2*Math.PI , 0, 6.25]}
+        />
+      </mesh>
+
+    </Float>
   )
 }
 
-export default Ball
+const CanvasBall = ({icon})=>{
+
+  return(
+    <Canvas
+    frameloop='demand'
+    gl = {{preserveDrawingBuffer: true}}    
+    >
+      <Suspense
+      fallback = {<CanvasLoader/>}
+      >
+        <OrbitControls 
+        enableZoom={false} 
+        enableRotate={true} 
+        enablePan={false}
+      />
+  
+
+        <Ball key={icon} icon={icon}/>
+      </Suspense>
+      <Preload all/>
+    </Canvas>
+  )
+}
+
+export default CanvasBall;
